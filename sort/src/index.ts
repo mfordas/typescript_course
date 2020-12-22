bca8a7a25455c88337a82a4ad12f58734f61e051
Mateusz Fordas <mateusz.fordas@gmail.com>
2020-12-22 20:08:16 +0100

    Basic implementation of buble sort algorithm

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hi there!!!!');
+class Sorter {
+    collection: number[];
+
+    constructor (collection: number[]) {
+        this.collection = collection;
+    };
+
+    sort(): void {
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length -i - 1; j++) {
+                if (this.collection[j] > this.collection[j+1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j+1];
+                    this.collection[j+1] = leftHand;
+                }
+            }
+        }
+    }
+};
+
+const sorter = new Sorter([18, 5, -5, 0]);
+
+sorter.sort();
+
+console.log(sorter);
