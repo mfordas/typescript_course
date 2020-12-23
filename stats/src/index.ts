0a3173fa38e038af1d916f18e36df820ea4ae7d2
Mateusz Fordas <mateusz.fordas@gmail.com>
2020-12-23 17:56:06 +0100

    Added csv files reader and date parser

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,20 @@
-console.log('works');
+import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
+const reader = new CsvFileReader('football.csv');
+
+reader.read();
+
+console.log(reader.data[0][0]);
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (
+    (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) ||
+    (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
+  ) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United won ${manUnitedWins} games`);
