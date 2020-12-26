3fa51c17b791ca1d709abae58e3439fb8d9d183c
Mateusz Fordas <mateusz.fordas@gmail.com>
2020-12-26 13:25:26 +0100

    Generics constraints

--- features/generics.ts
+++ features/generics.ts
@@ -45,3 +45,29 @@ const printAnything = <T>(arr: T[]): void => {
 };
 
 printAnything<string>(['a', 'b', 'c']);
+
+// Generic Constraints
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+const printHousesOrCars = <T extends Printable>(arr: T[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+};
+
+printHousesOrCars<House>([new House(), new House()]);
