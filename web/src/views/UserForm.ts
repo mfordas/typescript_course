0dd95c736ab14476c74d80476fd70cf251e9b52e
Mateusz Fordas <mateusz.fordas@gmail.com>
2020-12-27 22:16:01 +0100

    Added User Form View

---
+++ web/src/views/UserForm.ts
@@ -0,0 +1,49 @@
+export class UserForm {
+  constructor(public parent: Element) {}
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseover:h1': this.onHeaderHover,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('Hi there');
+  }
+
+  onHeaderHover(): void {
+    console.log('Works');
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  template(): string {
+    return `
+        <div>
+        <h1>User Form</h1>
+        <input />
+        <button>Click</button>
+        </div>
+        `;
+  }
+
+  render(): void {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
+
+    this.parent.append(templateElement.content);
+  }
+}
