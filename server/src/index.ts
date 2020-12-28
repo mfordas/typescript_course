f2dd1c90e0f44cf6bf40f4066666ecbaf16de6c2
Mateusz Fordas <mateusz.fordas@gmail.com>
2020-12-28 18:00:51 +0100

    Added login and logout routes, added auth verification

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 import { router } from './routes/login';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['asdfk'] }));
 
 app.use(router);
 
