cb253e10ce14d4c42272bae4047bf170d4cb9edf
Mateusz Fordas <mateusz.fordas@gmail.com>
2020-12-29 11:16:14 +0100

    First implementation of routes with metadata and decorators

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,16 @@ import cookieSession from 'cookie-session';
 
 import { router } from './routes/login';
 
+import './controllers/LoginController';
+
+import { AppRouter } from './AppRouter';
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['asdfk'] }));
 
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => console.log(`Listening on port 3000`));
