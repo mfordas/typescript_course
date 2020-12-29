8c846fbd4bfa8780838eafeb501d2a9532b76c48
Mateusz Fordas <mateusz.fordas@gmail.com>
2020-12-29 18:59:27 +0100

    Added todos reducer

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -3,10 +3,15 @@ import { Dispatch } from 'redux';
 
 import { ActionTypes } from './types';
 
-interface Todo {
+export interface Todo {
   id: number;
   title: string;
   completed: boolean;
+}
+
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
 }
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
@@ -14,7 +19,7 @@ const url = 'https://jsonplaceholder.typicode.com/todos';
 export const fetchTodos = () => async (dispatch: Dispatch) => {
   const response = await axios.get<Todo[]>(url);
 
-  dispatch({
+  dispatch<FetchTodosAction>({
     type: ActionTypes.fetchTodos,
     payload: response.data,
   });
