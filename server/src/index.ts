91050b25db3e48ef807f1e2f20fe40c2297c01b2
Mateusz Fordas <mateusz.fordas@gmail.com>
2020-12-29 16:36:53 +0100

    Added root controller

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { router } from './routes/login';
-
 import './controllers/LoginController';
+import './controllers/RootController';
 
 import { AppRouter } from './AppRouter';
 
@@ -13,7 +12,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ['asdfk'] }));
 
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => console.log(`Listening on port 3000`));
