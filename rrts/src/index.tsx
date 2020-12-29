a9bcc38debc5f6550f19e343abb3709ad7637486
Mateusz Fordas <mateusz.fordas@gmail.com>
2020-12-29 18:45:21 +0100

    Added fetch action and types file

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,51 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-interface AppProps {
-    color: string;
-}
+import { App } from './Components/App';
+import { reducers } from './reducers';
 
-interface State {
-    counter: number;
-}
-
-// Class component example
-
-// const App = (props: AppProps): JSX.Element => {
-//     return (
-//         <div>
-//             {props.color}
-//         </div>
-//     )
-// }
-
-class App extends React.Component<AppProps, State> {
-    constructor(props: AppProps) {
-        super(props);
-        this.state = {
-            counter: 0
-        };
-    }
-
-    onIncrement = (): void => {
-        this.setState({ counter: this.state.counter+1});
-    }
-
-    onDecrement = (): void => {
-        this.setState({ counter: this.state.counter-1});
-    }
+const store = createStore(reducers, applyMiddleware(thunk));
 
 
-    render() {
-        return (
-            <div>
-                <button onClick={this.onIncrement}>Increment</button>
-                <button onClick={this.onDecrement}>Decrement</button>
-                {this.state.counter}
-            </div>
-        )
-    }
 
-};
-
-ReactDOM.render(<App color="bbbbb" />, document.querySelector('#root'));
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+ document.querySelector('#root'));
