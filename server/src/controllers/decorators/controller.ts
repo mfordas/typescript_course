364d0b3d15f00120d90ec02912187d7190d6b89f
Mateusz Fordas <mateusz.fordas@gmail.com>
2020-12-29 11:38:27 +0100

    Added enums for routes methods and metadata

--- server/src/controllers/decorators/controller.ts
+++ server/src/controllers/decorators/controller.ts
@@ -1,5 +1,8 @@
 import { AppRouter } from '../../AppRouter';
 
+import { Methods } from './Methods';
+import { MetadataKeys } from './MetadataKeys';
+
 export function controller(routePrefix: string) {
   return function (target: Function) {
     const router = AppRouter.getInstance();
@@ -7,10 +10,20 @@ export function controller(routePrefix: string) {
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
 
-      const path = Reflect.getMetadata('path', target.prototype, key);
+      const path = Reflect.getMetadata(
+        MetadataKeys.path,
+        target.prototype,
+        key
+      );
+
+      const method: Methods = Reflect.getMetadata(
+        MetadataKeys.method,
+        target.prototype,
+        key
+      );
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
       }
     }
   };
